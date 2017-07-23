ba2c92cbbe3f9a0b97c2a61bedbe788b61d2c8e9
Falk Hoppe <falkhoppe81@gmail.com>
2017-07-23 13:01:45 +0200

    add default-layout and fix bundling

---
+++ example/views/default-layout.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import { registerMacro, createElement } from "complate-stream";
+
+registerMacro("default-layout", (...children) => {
+	return <html lang="en">
+		<head>
+			<meta charset="UTF-8"/>
+			<title>Document</title>
+		</head>
+		<body>
+			{children}
+		</body>
+	</html>;
+});
+export default { hello: true };
--- example/views/index.jsx
+++ example/views/index.jsx
@@ -1,8 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import renderer, { registerMacro, createElement } from "complate-stream";
+import "./default-layout";
 
 registerMacro("site-index", ({ title }) => {
-	return <default-layout title={title}>
+	return <default-layout>
 		<h1>{title}</h1>
 	</default-layout>;
 });
