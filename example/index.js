8b37b08bc9f16e6435280f00bd215a62a8ec5d46
Falk Hoppe <falkhoppe81@gmail.com>
2017-07-24 14:51:56 +0200

    refresh the given renderer in dev-env
    This allows the complate rendering process to be up-2-date with
    re-created renderer-bundles once they changed.

--- example/index.js
+++ example/index.js
@@ -3,10 +3,15 @@ const path = require("path");
 const express = require("express");
 const complate = require("complate-express");
 
-const bundle = require(path.resolve(__dirname, "dist", "bundle.js"));
 const app = express();
 
-app.use(complate(bundle));
+// just hand in the complate middleware, to register the complate
+// rendering function. In dev-env the bundle is not cached, so that
+// it can be updated between incoming requests.
+//
+// This behavior is disabled in `NODE_ENV=production` or can be disabled
+// by passing in options with cache: true (default is false).
+app.use(complate(path.resolve(__dirname, "dist", "bundle.js"), { cache: false }));
 
 app.get("/", (req, res) => {
 	return res.complate("site-index", { title: "Hello World!" });
--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@
 
 const WritableStream = require("./writable_stream");
 
-module.exports = renderer => {
+module.exports = rendererPath => {
 	return (req, res, next) => {
+		const renderer = typeof rendererPath === "function" ?
+			rendererPath	:
+			resolveRenderer(res.app, rendererPath, { cache: true });
+
 		res.complate = (tag, params) => {
 			let stream = new WritableStream(res);
 			renderer(stream, tag, params);
@@ -13,3 +17,17 @@ module.exports = renderer => {
 		next();
 	};
 };
+
+function resolveRenderer(app, rendererPath, { cache } = {}) {
+	// by default the require cache is disabled for dev environment,
+	// so the given renderer is updated on each request.
+	// This enables a watch/compile setup for re-creating the renderer
+	// and its components.
+	//
+	// Set `cache` to true, if you want to cache the renderer also in dev-mode.
+	if(!cache || !app.enabled("view cache")) {
+		delete require.cache[rendererPath];
+	}
+
+	return require(rendererPath);
+}
